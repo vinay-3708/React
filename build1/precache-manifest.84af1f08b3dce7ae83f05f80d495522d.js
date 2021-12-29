0073bef37f9301690654d7f3ab2982927b14b5f5
vinay-3708 <85564385+vinay-3708@users.noreply.github.com>
2021-12-29 11:44:05 +0530

    Add files via upload
    3

---
+++ build1/precache-manifest.84af1f08b3dce7ae83f05f80d495522d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48abf29a01bc08202217866d884223eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f43d658d9cc7aaa9fe",
+    "url": "/static/css/main.07c01401.chunk.css"
+  },
+  {
+    "revision": "bcfcb754ab93cb57845e",
+    "url": "/static/js/2.48590ec0.chunk.js"
+  },
+  {
+    "revision": "2ccdf9a65abf8f63f8aa553c3dc35f7d",
+    "url": "/static/js/2.48590ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f43d658d9cc7aaa9fe",
+    "url": "/static/js/main.82c62e41.chunk.js"
+  },
+  {
+    "revision": "a238ee0e0cda1386029c",
+    "url": "/static/js/runtime-main.e7ec637c.js"
+  },
+  {
+    "revision": "14fb01c406a39aab06275f5578c7316f",
+    "url": "/static/media/Core-icons.14fb01c4.woff"
+  },
+  {
+    "revision": "1b9af651cbf6b3f133faf2bede0e5266",
+    "url": "/static/media/Core-icons.1b9af651.ttf"
+  },
+  {
+    "revision": "4611bc4557599035d912ff07366ad002",
+    "url": "/static/media/Core-icons.4611bc45.eot"
+  },
+  {
+    "revision": "b0d01864ab94dc99bc25088891a6fbd4",
+    "url": "/static/media/Core-icons.b0d01864.svg"
+  },
+  {
+    "revision": "b2622e38f246209fb933e8b8a6ee76ed",
+    "url": "/static/media/PTSerif-Bold.b2622e38.woff"
+  },
+  {
+    "revision": "54f926c7e2cffb824547c75bc5ac0b5e",
+    "url": "/static/media/SourceSansPro-Bold.54f926c7.woff"
+  },
+  {
+    "revision": "ccdb76a033026dbdc3f08c50f4a5263f",
+    "url": "/static/media/SourceSansPro-Light.ccdb76a0.woff"
+  },
+  {
+    "revision": "fdc0354668675e460e18d8e6b880e634",
+    "url": "/static/media/SourceSansPro-Regular.fdc03546.woff"
+  },
+  {
+    "revision": "392cb2e0295e00036c634860a8f0cbad",
+    "url": "/static/media/checkbox-checked.392cb2e0.svg"
+  },
+  {
+    "revision": "abf2d2b04c7833a4fedc9c07bdb5a642",
+    "url": "/static/media/checkbox-unchecked.abf2d2b0.svg"
+  },
+  {
+    "revision": "171ce4e4cb28e074f5c9967f7db949b3",
+    "url": "/static/media/info.171ce4e4.svg"
+  },
+  {
+    "revision": "b481a3cf15a5dfcab9382b80d8fdefb4",
+    "url": "/static/media/kk-logo.b481a3cf.svg"
+  },
+  {
+    "revision": "1a84a9c1102aa390f7d788a9a4f4603c",
+    "url": "/static/media/radio-button-checked.1a84a9c1.svg"
+  },
+  {
+    "revision": "8c6533e10367d0901e362f7d295e03b9",
+    "url": "/static/media/radio-button.8c6533e1.svg"
+  }
+]);
